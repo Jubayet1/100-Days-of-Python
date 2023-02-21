aee091a42ab206a511a0c8c05c916532728915a8
jubayet <jubayet0106@gmail.com>
2023-02-22 02:32:53 +0600

    update

---
+++ Day 02/BMI Calculator/main.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+intHeight = float(height)
+intWeight = float(weight)
+print(int(intWeight/intHeight**2))
