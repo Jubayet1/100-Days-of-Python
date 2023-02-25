0a1a9809ab5681f55c674db10ac40f0355b087e1
jubayet <jubayet0106@gmail.com>
2023-02-25 19:16:49 +0600

    challenge three

--- Day 07/main.py
+++ Day 07/main.py
@@ -1,19 +1,25 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
+#Create blanks
 display = []
-word_length = len(chosen_word)
 for _ in range(word_length):
     display += "_"
+end_of_game = False
 
-guess = input("Guess a letter: ").lower()
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
 
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
-print(display)
+    print(display)
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
